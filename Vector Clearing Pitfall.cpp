9cc46e64218e8e8b9bf08c2e12be8177586e9955
jiwon seo <64701875+jwseo4074@users.noreply.github.com>
2020-10-12 02:43:17 +0900

    Update Vector Clearing Pitfall.cpp

--- Vector Clearing Pitfall.cpp
+++ Vector Clearing Pitfall.cpp
@@ -13,4 +13,7 @@ int main() {
     cout <<  "After clear *vec.end()-1 = " << *(vec.end()-1) << endl ;
     cout <<  "After clear vec[4]= " << vec[4] << endl ;
 
+    // clear : 다 0으로 채우는 즉 , 값을 다 지우는게 아니라
+    // end()를 즉 , 마지막의 포인터를 젤 앞에꺼로 옮겨주는.
+    // clear 하고 pushback 하면 그게 begin()이 됨.
 } // end of main( )
