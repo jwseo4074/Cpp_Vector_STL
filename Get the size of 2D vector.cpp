24c9b865da5706c68fa7b878b333f1aafd3db8e2
jiwon seo <64701875+jwseo4074@users.noreply.github.com>
2020-10-12 02:38:22 +0900

    Create Get the size of 2D vector.cpp

---
+++ Get the size of 2D vector.cpp
@@ -0,0 +1,15 @@
+// vector::size
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+   vector< vector<int> > my_array { {1,2,3,4}, {4,5,7,8}, {9,10,11,12} }; /* 2D Array */
+
+   cout << "\n my_array.size()="  << my_array.size(); /* size of y */
+   cout << "\n my_array[0].size()=" << my_array[0].size(); /* size of x */
+
+
+
+  return 0;
+}
