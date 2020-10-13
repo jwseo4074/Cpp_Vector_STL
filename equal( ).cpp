9b54072678c6febc6571da8e53c54c57935a0f65
jiwon seo <64701875+jwseo4074@users.noreply.github.com>
2020-10-14 03:09:20 +0900

    Update equal( ).cpp

--- equal( ).cpp
+++ equal( ).cpp
@@ -16,6 +16,10 @@ cout << "\n vector 전체가 같은지 검사 : " ;
 
 cout << "\n vec3가 vec1( )일부와 같은지 검사. " ;
 equal(vec3.begin(), vec3.end(), vec1.begin() + 2) ? cout <<  "vec3 일치" : cout << " vec3 불일치"  ;
+  //첫번째 구간과 두번째 구간을 비교하는데
+  //첫번째 구간의 길이가 나왔으므로
+  //두번째 구간은 자동으로 길이 지정.
+  // 5 22 33 이랑 (길이 3) , 5부터 시작 ( 길이 3 ) 이랑
 
 cout << "\n vec4가 vec1( )일부와 같은지 검사. " ;
 equal(vec4.begin(), vec4.end(), vec1.begin() + 2) ? cout << " vec4  일치" : cout << " vec4  불일치"  ;
